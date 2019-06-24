3625ef96136891e7adfa058bd303dcf7d20ebd6e
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-06-24 12:38:20 +0300

    One instruction at a time

--- verilog/sh4a_decode.v
+++ verilog/sh4a_decode.v
@@ -12,121 +12,22 @@ module sh4a_decode(
     output reg dest_valid,
     output reg [3:0] dest_reg,
     output reg imm_valid,
-    output reg [31:0] imm
+    output reg [31:0] imm,
+    output reg [5:0] op
 );
+
+localparam NO_OP = 6'd0;
+localparam MULTIPLY = 6'd1;
+localparam ILLEGAL = 6'd63;
 
 always @(posedge clk) begin
     {insn_valid, insn_privileged, src1_valid, src2_valid, src3_valid, dest_valid, imm_valid} <= 'b0;
     casez (insn)
-        16'h0?02, 16'h0?12, 16'h0?22, 16'h0?32, 16'h0?42: begin // STC {SR, GBR, VBR, SSR, SGR}, Rn
-            {insn_valid, dest_valid} <= 2'h3;
-            dest_reg <= insn[11:8];
-        end
-        16'h0?02, 16'h0?03: begin // BSRF Rn
-            {insn_valid, src1_valid} <= 2'h3;
+        16'h0??7: begin // mul.l Rn, Rm
+            {insn_valid, src1_valid, src2_valid} <= 3'b111;
             src1_reg <= insn[11:8];
-        end
-        16'h0?23: begin // BRAF Rn
-            {insn_valid, src1_valid} <= 2'h3;
-            src1_reg <= insn[11:8];
-        end
-        16'h0?63: begin // MOVLI.L @Rm, R0
-            {insn_valid, src1_valid, dest_valid} <= 3'h7;
-            src1_reg <= insn[11:8];
-            dest_reg <= 4'd0;
-        end
-        16'h0?73: begin // MOVCO.L R0, @Rn
-            {insn_valid, src1_valid, src2_valid} <= 3'h7;
-            src1_reg <= 4'd0;
-            src2_reg <= insn[11:8];
-        end
-        // {PREF, OCBI, OCBP, OCBWB, PREFI, ICBI} @Rn
-        16'h0?83, 16'h0?93, 16'h0?A3, 16'h0?B3, 16'h0?D3, 16'h0?E3: begin 
-            {insn_valid, src1_valid} <= 2'h3;
-            src1_reg <= insn[11:8];
-        end
-        16'h0??4, 16'h0??5, 16'h0??6: begin // MOV.[BWL] Rm, @(R0, Rn)
-            {insn_valid, src1_valid, src2_valid, src3_valid} <= 4'hF;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-            src3_reg <= 4'd0;
-        end
-        16'h0??7: begin // MUL.L Rm, Rn
-            {insn_valid, src1_valid, src2_valid} <= 3'h7;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-        end
-        16'h0008, 16'h0018: begin // {CLRT, SETT}
-            {insn_valid} <= 1'h1;
-        end
-        16'h0028: begin // CLRMAC
-            {insn_valid} <= 1'h1;
-        end
-        16'h0038: begin // LDTLB
-            {insn_valid} <= 1'h1;
-        end
-        16'h0048, 16'h0058: begin // {CLRS, SETS}
-            {insn_valid} <= 1'h1;
-        end
-        16'h0009: begin // NOP
-            {insn_valid} <= 1'h1;
-        end
-        16'h0019: begin // DIV0U
-            {insn_valid} <= 1'h1;
-        end
-        16'h0?29: begin // MOVT Rn
-            {insn_valid, dest_valid} <= 2'h3;
-            dest_reg <= insn[11:8];
-        end
-        16'h000B: begin // RTS
-            {insn_valid} <= 1'h1;
-        end
-        16'h001B: begin // SLEEP
-            {insn_valid} <= 1'h1;
-        end
-        16'h002B: begin // RTE
-            {insn_valid} <= 1'h1;
-        end
-        16'h00AB: begin // SYNCO
-            {insn_valid} <= 1'h1;
-        end
-        16'h0??C, 16'h0??D, 16'h0??E: begin // MOV.[BWL] @(R0, Rm), Rn
-            {insn_valid, src1_valid, src2_valid, dest_valid} <= 4'hF;
-            src1_reg <= 4'd0;
             src2_reg <= insn[7:4];
-            dest_reg <= insn[11:8];
-        end
-        16'h0??F: begin // MAC.L @Rm+, @Rn+
-            {insn_valid, src1_valid, src2_valid} <= 3'h7;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-        end
-        16'h1???: begin // MOV.L Rm, @(disp*, Rn)
-            {insn_valid, src1_valid, src2_valid, imm_valid} <= 4'hF;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-            imm <= {26'b0, insn[3:0], 2'b0};
-        end
-        16'h2??0, 16'h2??1, 16'h2??2: begin // MOV.[BWL] Rm, @Rn
-            {insn_valid, src1_valid, src2_valid} <= 3'h7;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-        end
-        16'h2??4, 16'h2??5, 16'h2??6: begin // MOV.[BWL] Rm, @-Rn
-            {insn_valid, src1_valid, src2_valid} <= 3'h7;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-        end
-        16'h2??D: begin // XTRCT Rm, Rn
-            {insn_valid, src1_valid, src2_valid, dest_valid} <= 4'hF;
-            src1_reg <= insn[7:4];
-            src2_reg <= insn[11:8];
-            dest_reg <= insn[11:8];
-        end
-        16'hE???: begin // MOV #imm, Rn
-            {insn_valid, dest_valid, imm_valid} <= 3'h7;
-            dest_reg <= insn[11:8];
-            imm <= {{24{insn[7]}}, insn[7:0]};
+            op <= MULTIPLY;
         end
     endcase
 end
